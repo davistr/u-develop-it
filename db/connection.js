4d68a4b3bcda49b5e04b65ccbf5ab8548f6fa574
davistr <davistr1216@gmail.com>
2022-02-22 22:31:31 -0500

    added voter routes

---
+++ db/connection.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    // Your MySQL username,
+    user: process.env.DB_USER,
+    // Your MySQL password
+    password: process.env.DB_PW,
+    database: process.env.DB_NAME
+});
+
+module.exports = db;
+
+
+module.exports = db;
